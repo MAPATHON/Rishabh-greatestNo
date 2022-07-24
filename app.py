bf228c7df0c553a377094b7e09dfaadf6749f56f
RISHABH SINGH <96380110+MAPATHON@users.noreply.github.com>
2022-07-24 23:24:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
 import streamlit as st
 
-st.write("""
-# largest among 3 given numbers App
+st.title('This app divides the two numbers')
+a= st.number_input('Number A')
 
-This app predicts the credit card approval probablity
-""")
 
-a=st.number_input('Number_A')
-b=st.number_input('Number_B')
-c=st.number_input('Number_C')
-if (a>b && a>c):
-    greatest=st.write('A')
-elif(b>c && b>a):
-    greatest=st.write('B')
-else(c>a && c>b):
-    greatest=st.write('B')
-
-print(greatest)
+b =st.number_input('Number B')
+if b == 0:
+  a1=st.write(f'A/B = Undefined')
+else:
+  a1=st.write(f'A/B = {a/b}')
+print(a1)
