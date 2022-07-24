c0c64007e492d3144ff4704f390eccd274d710b7
RISHABH SINGH <96380110+MAPATHON@users.noreply.github.com>
2022-07-24 23:29:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 
-st.title('This app divides the two numbers')
-a= st.number_input('Number A')
-
-
-b =st.number_input('Number B')
-if (a>b):
-  a1=st.write(A)
+st.title('Greatest among 3 numbers')
+a = st.number_input('Enter first number')
+b = st.number_input('Enter second number')
+c = st.number_input('Enter third number')
+if (a >= b) and (a >= c):
+   result = a
+elif (b >= a) and (b >= c):
+   result = b
 else:
-  a1=st.write(B)
-print(a1)
+   result = c
+st.write('Greatest Number: ', result)
